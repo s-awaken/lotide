5c5cef6546fd04a4059752674c6312609030e5f3
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-17 20:53:40 +0000

    First README.md file added. As well as the first function assertEqual.js

---
+++ README.md
@@ -0,0 +1,5 @@
+# Syrym's Lotide Project
+
+## Summary
+
+This project is the first project to be completed at Lighthouse Bootcamp
---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  //Start assertion
+  if (actual === expected) {
+    return "✅✅✅ Assertion Passed: " + actual + " === " + expected;
+  } else {
+    return "🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected;
+  }
+};
+console.log(assertEqual("Lighthouse Labs", "Lighthouse Labs"));
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(12, 9));
