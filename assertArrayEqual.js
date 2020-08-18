3eb0872827211c9f4c3551be35a710a4f384b4d5
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-18 02:30:24 +0000

    Added Assertion of Array Equality

---
+++ assertArrayEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function (array_1, array_2) {
+  let equal = true;
+  for (let i = 0; i < array_1.length; i++) {
+    if (array_1[i] !== array_2[i]) {
+      equal = false;
+    }
+  }
+  return equal;
+};
+
+const assertArrayEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+console.log(assertArrayEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArrayEqual([1, 2, 3], [1, 3, 3]));
