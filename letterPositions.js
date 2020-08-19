df85c57ea0b97cac68dc0c58772d996ac36e1bc0
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-19 13:08:22 -0700

    letterPositions.js added

---
+++ letterPositions.js
@@ -0,0 +1,13 @@
+const letterPositions = function (sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+console.log(letterPositions("Hello World"));
