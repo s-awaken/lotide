16b94013c3327dd406533200495689d0bcf5dedd
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-19 12:36:38 -0700

    count letters added

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+const countLetters = function (string) {
+  const result = {};
+  string = string.split(" ").join("");
+  for (const char of string) {
+    if (result[char]) {
+      result[char] += 1;
+    } else {
+      result[char] = 1;
+    }
+  }
+  return result;
+};
+console.log(countLetters("lighthouse in the house"));
