86d4a44fd6151d705d9296fb810fcd20febfc8c5
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-19 17:15:20 +0000

    Finding a middle of an array added

---
+++ middle.js
@@ -0,0 +1,19 @@
+const middle = function (array) {
+  if (array.length <= 2) {
+    return [];
+  }
+  const newArray = [];
+  const midIndex = Math.floor(array.length / 2);
+  if (Number.isInteger(array.length / 2)) {
+    newArray.push(array[midIndex - 1], array[midIndex]);
+  } else {
+    newArray.push(array[midIndex]);
+  }
+  return newArray;
+};
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4])
