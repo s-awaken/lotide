f06ac1879eb5f06f93dabecd5677ed3a551b10b9
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-20 12:31:30 -0700

    Added map.js

---
+++ map.js
@@ -0,0 +1,12 @@
+const map = function (array, callback) {
+  const results = [];
+  for (const element in array) {
+    results.push(callback(element));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
