b7eed2d60756c82adc6bd1af49c39e862e909abf
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-21 23:26:55 +0000

    recursive eqObjects and eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,8 @@
 const eqArrays = function(array1, array2) {
-  let equal = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      equal = false;
-    }
-  }
-  return equal;
+    return Array.isArray(array1) && Array.isArray(array2) && array1.length === array2.length ?
+    array1.every((a, b) => (eqArrays(a, array2[b]))) :
+    (array1 === array2)
+  
 };
 
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
@@ -13,3 +10,7 @@ eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
--- eqObjects.js
+++ eqObjects.js
@@ -1,16 +1,16 @@
 const eqObjects = function (object1, object2) {
-  let keys = Object.keys(object1);
+  const keys = Object.keys(object1);
   if (keys.length !== Object.keys(object2).length) {
     return false;
   }
   return keys.every(k => eqObjects(object1[k], object2[k]));
 };
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// console.log(eqObjects(ab, ba)); // => true
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
 
-// const abc = { a: "1", b: "2", c: "3" };
-// console.log(eqObjects(ab, abc))
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc))
 ;
 console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
 
