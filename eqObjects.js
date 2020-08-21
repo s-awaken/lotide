5565e1914cb60ef3b7e72ae1cd04789478e624f3
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-21 23:01:28 +0000

    eqObjects using recusrion

--- eqObjects.js
+++ eqObjects.js
@@ -1,11 +1,18 @@
 const eqObjects = function (object1, object2) {
-  return Object.keys(object1).every((key) => object1[key] === object2[key])
-    ? Object.keys(object2).every((key) => object1[key] === object2[key])
-    : false;
+  let keys = Object.keys(object1);
+  if (keys.length !== Object.keys(object2).length) {
+    return false;
+  }
+  return keys.every(k => eqObjects(object1[k], object2[k]));
 };
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// console.log(eqObjects(ab, ba)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc));
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(eqObjects(ab, abc))
+;
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
